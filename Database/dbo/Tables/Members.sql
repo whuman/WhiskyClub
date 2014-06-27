e49db27048db6c06adaf8e1c9b40e6d31f9262ba
whuman <waynne@gmail.com>
2014-06-27 22:25:23 +0200

    Deleted these unnecessary files

--- Database/dbo/Tables/Members.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Members] (
-    [MemberId]     INT          IDENTITY (1, 1) NOT NULL,
-    [Name]         VARCHAR (50) NOT NULL,
-    [Version]      BINARY (8)   NOT NULL,
-    [InsertedDate] DATETIME     NOT NULL,
-    [UpdatedDate]  DATETIME     NOT NULL,
-    CONSTRAINT [PK_Members] PRIMARY KEY CLUSTERED ([MemberId] ASC)
-);
-
